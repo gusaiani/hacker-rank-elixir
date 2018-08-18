4d2213c3dc07ca24f22788e6587864a3ec1899fc
Gustavo Saiani <gustavo@poe.ma>
2018-08-18 19:06:15 -0300

    Make Mars Exploration solution more declarative

--- lib/mars_exploration.ex
+++ lib/mars_exploration.ex
@@ -8,27 +8,35 @@ defmodule MarsExploration do
   def main(str) do
     str
     |> String.graphemes()
-    |> countWrongChars()
+    |> count_wrong_chars()
   end
 
-  defp countWrongChars(list) do
-    base_string_length = String.length(@base_string)
+  defp count_wrong_chars(list) do
+    {_, wrong_char_count} =
+      Enum.reduce(list, {0, 0}, fn char, acc ->
+        {index, count} = acc
 
-    {_, wrong_char_count} = Enum.reduce(list, {0, 0}, fn (char, acc) ->
-      {index, wrong_char_count} = acc
+        new_count = update_count(index, char, count)
 
-      char_to_compare =
-        @base_string
-        |> String.at(rem(index, base_string_length))
-
-      wrong_char_count = case char_to_compare == char do
-        true -> wrong_char_count
-        false -> wrong_char_count + 1
-      end
-
-      {index + 1, wrong_char_count}
-    end)
+        {index + 1, new_count}
+      end)
 
     wrong_char_count
+  end
+
+  defp update_count(index, char, count) do
+    char_to_compare = get_char_to_compare(index)
+
+    case char_to_compare == char do
+      true -> count
+      false -> count + 1
+    end
+  end
+
+  defp get_char_to_compare(index) do
+    base_string_length = String.length(@base_string)
+    char_position = rem(index, base_string_length)
+
+    String.at(@base_string, char_position)
   end
 end
